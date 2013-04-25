a6b7137d83d8064759d6ee7848c4d9adeebd958d
larissafelix.felix <larissafelix.felix@gmail.com>
2013-04-25 15:55:30 +0000

    - Configuração do ambiente; - Ajuste na base de dados; - Validação no cliente.

--- NutriIF/database/connect.class.php
+++ NutriIF/database/connect.class.php
@@ -11,6 +11,7 @@ class connect_class {
         $this->db = new db_class;
 
         if (!$this->db->connect('127.0.0.1', 'nutrif_user', 'nutr1f_us3r', 'nutrif', false)) {
+                   echo "Não conectou no banco!";
             $this->db->print_last_error(false);
         }
     }
