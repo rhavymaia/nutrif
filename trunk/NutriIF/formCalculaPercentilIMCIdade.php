4068d6ddd5b34c5fccd612e8ba164555580e812e
lorranyand@gmail.com <lorranyand@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2013-05-24 17:20:39 +0000

    acrescentar a página de busca

--- trunk/NutriIF/formCalculaPercentilIMCIdade.php
+++ trunk/NutriIF/formCalculaPercentilIMCIdade.php
@@ -1,5 +1,15 @@
 <?php
-/*
+ // Cabeçalho e menu da página html.
+    require_once 'template/header.php';
+    ?>
+
+    <form action="resultado.php" method="post">
+
+		Digite a matrícula a ser procurada: 
+<input name="MatriculaDeBusca" type="text">
+			<input type="submit" value="Buscar">
+
+<?php /*
 consultar o paciente na base de dados na tabela entrevistado;
 
 data nascimento: YYYY-dd-mm;
@@ -16,4 +26,8 @@ calcular a idade em meses;
 
 buscar o percentil na base de dados: sexo, idade, imc
  */
+?>
+<?php 
+    // Rodapé da página html.
+    require_once 'template/footer.php';
 ?>
