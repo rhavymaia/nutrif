dffb3719d927e69e1c9a1409acbbb1cc9cc48147
lorranyand <lorranyand@gmail.com>
2013-05-25 18:06:52 +0000

    conexão com o Banco de dados

--- NutriIF/formCalculaPercentilIMCIdade.php
+++ NutriIF/formCalculaPercentilIMCIdade.php
@@ -3,29 +3,16 @@
     require_once 'template/header.php';
     ?>
 
-    <form action="resultado.php" method="post">
+    <form action="TrataCalculaPercentilIMCidade.php" method="post">
 
 		Digite a matrícula a ser procurada: 
 <input name="MatriculaDeBusca" type="text">
 			<input type="submit" value="Buscar">
+                        
+       
 
-<?php /*
-consultar o paciente na base de dados na tabela entrevistado;
+<?php 
 
-data nascimento: YYYY-dd-mm;
-
-sexo: 1 (masculino) e 2 (feminino)
-
-peso: float;
-
-altura: float;
-
-calcular o IMC;
-
-calcular a idade em meses;
-
-buscar o percentil na base de dados: sexo, idade, imc
- */
 ?>
 <?php 
     // Rodapé da página html.
