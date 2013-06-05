1f6a3d979a3b41742e261203abe6ac5e16e07c68
rhavy.maia@gmail.com <rhavy.maia@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2013-06-05 22:36:00 +0000

    - Ajuste na tabela tb_entrevistado; - Adicionar o campo Quantidade de atividade física por semana no formulário de cadastro Antropométrico

--- trunk/NutriIF/database/connect.class.php
+++ trunk/NutriIF/database/connect.class.php
@@ -10,7 +10,7 @@ class connect_class {
 
         $this->db = new db_class;
 
-        if (!$this->db->connect('127.0.0.1', 'nutrif_user', 'nutr1f_us3r', 'nutrif', false)) {
+        if (!$this->db->connect('localhost', 'nutrif_user', 'nutr1f_us3r', 'nutrif', false)) {
                    echo "Não conectou no banco!";
             $this->db->print_last_error(false);
         }
