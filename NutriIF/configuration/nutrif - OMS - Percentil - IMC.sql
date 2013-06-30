6a422a8181bbd2be96abd76cfc5673c2aca937f6
rhavy.maia <rhavy.maia@gmail.com>
2013-06-30 14:24:20 +0000

    - Selecionar entrevistado e calcular percentil.

--- NutriIF/configuration/nutrif - OMS - Percentil - IMC.sql
+++ NutriIF/configuration/nutrif - OMS - Percentil - IMC.sql
@@ -3,14 +3,7 @@ CREATE TABLE IF NOT EXISTS tb_imc_percentil (
   cd_fator int(11) NOT NULL, -- Código da tabela Ex: (1) - Peso (kg) para idade (meses).
   tp_sexo char(1) NOT NULL,
   vl_fator double NOT NULL, -- Valor do Fator: Idade, comprimento, estatura.
-  nr_p_01 double NOT NULL,  
-  nr_p_3 double NOT NULL,
-  nr_p_5 double NOT NULL,
-  nr_p_10 double NOT NULL,
-  nr_p_15 double NOT NULL,
-  nr_p_50 double NOT NULL,
-  nr_p_85 double NOT NULL,
-  nr_p_97 double NOT NULL,
-  nr_p_999 double NOT NULL,
+  cd_percentil int(11) NOT NULL,  
+  vl_imc_percentil double NOT NULL,
   PRIMARY KEY (`cd_imc_percentil`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0;
