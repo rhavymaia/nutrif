6d76e204c326bec457e6a2ffe970374041a5ea44
rhavy.maia <rhavy.maia@gmail.com>
2013-07-01 11:46:31 +0000

    - Selecionar entrevistado e calcular percentil.

--- NutriIF/database/dao.class.php
+++ NutriIF/database/dao.class.php
@@ -4,21 +4,57 @@ require_once ('database/connect.class.php');
 
 class dao_class {
 
-    var $db;
-    
+    var $db;    
 
+    /**
+     * Construtor da classe.
+     */
     function dao_class() {
         $conn = new connect_class;
         $this->db = $conn->database();
     }
 
-    public function carregaMunicipio() {
-
-        $r = $this->db->select("SELECT cd_municipio, nm_municipio FROM tb_municipio");
-
-        return $this->db->get_rows($r);
+    /**
+     * Consultar tabela de percentil.
+     * 
+     * @param type $matricula
+     */
+    public function selectPercentil($matricula){
+        
+        // Calcular IMC do entrevistado.
+        $entrevistado = selectEntrevistado($matricula);
+        
+        // Verificar o retorno de somente um registro. Ele deve ser único.
+        $peso = $entrevistado['nr_peso'];
+        // ... capturar os outros valores.
+        
+        // Verificar o Percentil.
+    }
+    
+    /**
+     * Recuperação informações da avaliação antropométrica para cálcular o 
+     * percentil do IMC
+     * 
+     * @param type $matricula
+     * @return type
+     */
+    public function selectEntrevistado($matricula) {
+        
+        $select = "SELECT nr_matricula, dt_nascimento, nr_peso, nr_altura, tp_sexo ".
+                "FROM tb_entrevistado ".
+                "WHERE nr_matricula = ".$matricula;
+        
+        $r = $this->db->select($select);
+        
+        return $this->db->get_row($r);
     }
 
+    /**
+     * Inserir entrevistados da avaliação antropométrica.
+     * 
+     * @param type $data
+     * @return type
+     */
     public function inserirEntrevistado($data) {
 
         $id = $this->db->insert_array('tb_entrevistado', $data);
