e8d373a901232b2985208cfed6e170117843a642
larissafelix.felix@gmail.com <larissafelix.felix@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2013-08-14 16:47:37 +0000

    - adicionando a variável session

--- trunk/NutriIF/trataLogin.php
+++ trunk/NutriIF/trataLogin.php
@@ -5,14 +5,13 @@ session_start();
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */ 
-        $senha = $_POST['senha'];
-        $usuario = $_POST['usuario'];
-        
-        if (($senha == 'nutr1f_us3r') && ($usuario == 'nutrif_user')){
-            header("location: formRegistroAntropometrico.php"); 
-        }else{
-            $_SESSION['erro_login'] = "Senha inválida!";
-            header("location: login.php");  
-        }
+   $usuario = $_POST['nome'];
+   $senha = $_POST['senha'];
+   
+   if (($usuario == "nutrif_user") && ($senha == "nutr1f_us3r")){
+       echo("login realizado com sucesso!");
+   }else{
+       echo ("nome do usuário ou senha incorreto");
+   }
    
 ?>
