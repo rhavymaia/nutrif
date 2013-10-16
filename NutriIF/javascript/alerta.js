42ecc1c0a5980c238664e31554fb3496376d7e39
jonatha.kennedy1 <jonatha.kennedy1@gmail.com>
2013-10-16 16:56:49 +0000

    criação das páginas: formCalculaVCTVET.php e trataCalculaVCTVET.php para o cálculo do valor calórico e energético total.

---
+++ NutriIF/javascript/alerta.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Funcão para Alerta
+    function sucesso(){
+	alert("Realizado com sucesso!");
+    }
