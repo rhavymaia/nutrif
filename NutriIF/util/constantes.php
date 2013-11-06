18f274817e99520f84763680a6f74212fdc0b26d
jonatha.kennedy1 <jonatha.kennedy1@gmail.com>
2013-11-06 18:43:34 +0000

    Questionário

--- NutriIF/util/constantes.php
+++ NutriIF/util/constantes.php
@@ -14,7 +14,9 @@
        // Título do cálculo para VCT e VET
     define('TL_CALCULA_VCT_VET', 'Cálculo de VCT e VET');    
     
-    define('TL_LISTAGEM', 'Listagem'); 
+    define('TL_QUESTIONARIO', 'Questionário');
+    
+    define('TL_LISTAGEM', 'Listagem');  
     
     define('TL_RELATORIO', 'Relatório'); 
     
@@ -35,6 +37,5 @@
     define ('PERFIL_OBESO_MORBIDO',"Obeso mórbido");
     
     define ('PERFIL_MAGREZA',"Magreza");
-    
     
 ?>
