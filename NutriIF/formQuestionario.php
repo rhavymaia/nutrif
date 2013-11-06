b13269fd97743cb6d91f485c3ea1d4c39e1efb61
jonatha.kennedy1 <jonatha.kennedy1@gmail.com>
2013-11-06 18:43:55 +0000

    Questionário

---
+++ NutriIF/formQuestionario.php
@@ -0,0 +1,71 @@
+<?php
+require_once ('util/constantes.php');
+require_once ('validate/erro.php');
+require_once ('validate/validate.php');
+// Cabeçalho e menu da página html.
+require_once 'template/header.php';
+
+if (!isset($_SESSION['id'])) {
+    echo '<script language="javascript" type="text/javascript">';
+    echo 'window.alert("Área restrita, realize o Login!");';
+    echo 'window.location.href="login.php";';
+    echo '</script>';
+}
+?>
+
+<?php
+if (isset($_SESSION['id'])) {
+    echo "<div class='caixa_login'>";
+    echo "<div id='centralizar'>";
+    echo "<img src='images/user.png'>Olá, " . $_SESSION['id'];
+    echo "<a href='logout.php'> &nbsp Logout</a>";
+    echo "</div>";
+    echo "</div>";
+}
+?> 
+u
+<div class="container">
+    <div id="letras">
+        <p>
+        <h1>
+            <?php
+            echo TL_QUESTIONARIO;
+            ?>
+        </h1>
+        </p>
+    </div>
+    <ul id="erro">
+        <!-- Lista de erros na validação -->
+        <?php
+        isset($_SESSION['erro']) && sizeof($_SESSION['erro']) > 0 ? ErroMatricula($_SESSION['erro']) : VAZIO;
+        ?>                    
+    </ul>
+   
+       <form action="trataQuest.php" method="POST">
+
+        <label for="matricula"><em>*</em>Digite a matrícula a ser procurada:
+            <input type="text" name="matricula" onFocus="this.className = 'select'" 
+                   onBlur="this.className = 'normal'" value= "<?php
+                   echo(isset(
+                           $_SESSION['matricula']) ?
+                           $_SESSION['matricula'] : VAZIO);
+                   ?>"
+                   /> 
+        </label>
+        <input type="submit" value="Buscar"/>
+        <input type="reset" value="Limpar"/>
+    </form>
+        <div class="clear"></div>
+        <div class="container">
+            <div id="centralizares">
+            </div>
+            <div class="clear"></div>
+
+        </div>
+   
+
+    <?php
+// Rodapé da página html.
+    unset ($_SESSION['erro']);
+    require_once 'template/footer.php';
+    ?>
