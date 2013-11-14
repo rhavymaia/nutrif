6d93bf53545ccf4a29c45d64171694bc9f83e915
rhavy.maia@gmail.com <rhavy.maia@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2013-11-14 01:41:33 +0000

    - Ajuste dos formulário de percentil e VCT.

--- trunk/NutriIF/formCalculaVCT.php
+++ trunk/NutriIF/formCalculaVCT.php
@@ -8,46 +8,41 @@ require_once 'template/headerForm.php';
 
 <div class="container">
     <div id="letras">
-        <p> 
-            <h1>
-                <?php
-                    echo TL_CALCULO_VCT;
-                ?>
-            </h1>
-        </p>
+        <h1>
+            <?php
+                echo TL_CALCULO_VCT;
+            ?>
+        </h1>
     </div>
     <ul id="erro">
         <!-- Lista de erros na validação -->
         <?php
-        isset($_SESSION['erro']) && sizeof($_SESSION['erro']) > 0 ? ErroMatricula($_SESSION['erro']) : VAZIO;
+            isset($_SESSION['erro']) && sizeof($_SESSION['erro']) > 0 ? ErroMatricula($_SESSION['erro']) : VAZIO;
         ?>                    
     </ul>
     <form action="trataCalculaVCT.php" method="POST">
-
-        <label for="matricula"><em>*</em>Digite a matrícula a ser procurada:
-            <input type="text" name="matricula" onFocus="this.className = 'select'" onBlur="this.className = 'normal'" value= "<?php
-            echo(isset(
-                    $_SESSION['matricula']) ?
-                    $_SESSION['matricula'] : VAZIO);
-            ?>"
+        <label for="matricula">
+            <em>*</em>Digite a matrícula a ser procurada:
+            <input type="text" name="matricula" onFocus="this.className='select'" onBlur="this.className='normal'" 
+                   value="<?php echo(isset($_SESSION['matricula']) ? $_SESSION['matricula'] : VAZIO);?>"/>
         </label>
         
         <input type="submit" value="Buscar"/>
         <input type="reset" value="Limpar"/>
     </form>
-      
-<div class="container">      
-    <?php 
-        if (isset($_SESSION['vct'])) {    
-             echo '<div class="caixa_azul">';   
-             echo "<p>Valor Calórico Total (VCT): " . $_SESSION['vct'] . "</p>"; 
-         }
-         
-         unset($_SESSION['vct']);
-         unset($_SESSION['erro']);
+</div>
+
+<div class="caixa_azul">     
+    <?php
+        if (isset($_SESSION['vct'])) {
+            echo "<p>Valor Calórico Total (VCT): " . $_SESSION['vct'] . "</p>";
+        }
+
+        unset($_SESSION['vct']);
+        unset($_SESSION['erro']);
     ?>
 </div>
-</div>  
+
 <?php
 // Rodapé da página html.
 require_once 'template/footer.php';
