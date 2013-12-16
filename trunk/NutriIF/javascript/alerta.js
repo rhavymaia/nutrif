11031f733d0261293f9146ac5777cd63469ddc47
jonatha.kennedy1@gmail.com <jonatha.kennedy1@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2013-12-16 21:13:35 +0000

    Slides de fotos na página principal

--- trunk/NutriIF/javascript/alerta.js
+++
@@ -1,9 +0,0 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-// Funcão para Alerta
-    function sucesso(){
-	alert("Realizado com sucesso!");
-    }
