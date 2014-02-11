70a2c72c5b5fdc957faf01d70ab268dfe59c8158
larissafelix.felix@gmail.com <larissafelix.felix@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2014-02-11 14:53:45 +0000

    - Relatório dos entrevistados.

--- trunk/NutriIF/relatorio.php
+++ trunk/NutriIF/relatorio.php
@@ -1,32 +1,8 @@
 <?php
-require_once ('util/constantes.php');
-require_once ('validate/erro.php');
-require_once ('util/date.php');
-require_once ('validate/validate.php');
-require_once ('database/dao.class.php');
-
-
-// Cabeçalho e menu da página html.
-require_once 'template/header.php';
-
-if (!isset($_SESSION['id'])) {
-    echo '<script language="javascript" type="text/javascript">';
-    echo 'window.alert("Área restrita, realize o Login!");';
-    echo 'window.location.href="login.php";';
-    echo '</script>';
-}
+    require_once ('database/dao.class.php');
+    require_once ('validate/validate.php');
+    require_once ('util/constantes.php');
 ?>
-
-<?php
-if (isset($_SESSION['id'])) {
-    echo "<div class='caixa_login'>";
-    echo "<div id='centralizar'>";
-    echo "<img src='images/user.png'>Olá, " . $_SESSION['id'];
-    echo "<a href='logout.php'> &nbsp Logout</a>";
-    echo "</div>";
-    echo "</div>";
-}
-?> 
 
 <div class="container">
     <div id="letras">
@@ -50,8 +26,6 @@ if (isset($_SESSION['id'])) {
 
             $rowTodosEntrevistados = $dao->selectEntrevistados();
             
-            echo $rowTodosEntrevistados['nr_matricula'];
-    
             /*
              *  Está aparecendo esse erro Rhavy: Fatal error: Call to undefined method dao_class::selectEntrevistados() in C:\wamp\www\NutrIF\relatorio.php on line 51
              * 
@@ -60,16 +34,6 @@ if (isset($_SESSION['id'])) {
                  
                 echo "Matricula: ".$entrevistado['nr_matricula'];
             }
-            
-            
-        /*if (isset($_SESSION['qtd'])) {
-                echo ("<p> Maiores de 19 anos: ".$_SESSION['qtd']."</p>"); 
-                echo ("<p> Relação de Magreza: ".$_SESSION['qtdMagros']."</p>");
-                echo ("<p> Relação de Eutrofia: ".$_SESSION['qtdEutroficos']."</p>");
-                echo ("<p> Relação de Sobrepeso: ".$_SESSION['qtdSobrepeso']."</p>");
-                echo ("<p> Relação de Obesidade: ".$_SESSION['qtdObesos']."</p>");
-                echo ("<p> Relação de Obesidade Mórbida: ".$_SESSION['qtdObesosMorbidos']."</p>");
-            }*/
         ?>
 
     </form>
