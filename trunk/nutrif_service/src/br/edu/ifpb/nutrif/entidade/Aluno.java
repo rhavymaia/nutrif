e20eaa3cb4751b45e9a461c4d79df35436e7bbcc
rhavy.maia@gmail.com <rhavy.maia@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2014-07-22 14:46:43 +0000

    Commit Inicial.

---
+++ trunk/nutrif_service/src/br/edu/ifpb/nutrif/entidade/Aluno.java
@@ -0,0 +1,9 @@
+package br.edu.ifpb.nutrif.entidade;
+
+public class Aluno {
+
+	public Aluno() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ trunk/nutrif_service/src/br/edu/ifpb/nutrif/entidade/Nutricionista.java
@@ -0,0 +1,9 @@
+package br.edu.ifpb.nutrif.entidade;
+
+public class Nutricionista {
+
+	public Nutricionista() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
