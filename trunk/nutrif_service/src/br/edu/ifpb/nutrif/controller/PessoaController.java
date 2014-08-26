5db7009fe086c942e6fae55efc40a8103f828534
rhavy.maia@gmail.com <rhavy.maia@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2014-08-26 12:01:18 +0000

    Exemplo servidor Rest

---
+++ trunk/nutrif_service/src/br/edu/ifpb/nutrif/controller/PessoaController.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.nutrif.controller;
+
+import br.edu.ifpb.nutrif.entidade.Pessoa;
+
+public class PessoaController {
+
+	public PessoaController() {}
+	
+	public static Pessoa loginPessoa(String email, String password){
+		return null;		
+	}
+
+}
