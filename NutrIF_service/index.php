a9f429535fc19a47e0412bdb3668275b7d8d272d
rhavy.maia <rhavy.maia@gmail.com>
2014-08-27 15:05:22 +0000

    WebService NutrIF

---
+++ NutrIF_service/index.php
@@ -0,0 +1,44 @@
+<?php
+
+    // Serviços
+    require '../Slim/Slim/Slim.php';
+    \Slim\Slim::registerAutoloader();
+
+    $slim = new \Slim\Slim();
+    
+    $slim->response()->header('Content-Type', 'application/json;charset=utf-8');
+    
+    $slim->get('/verificar', 'verificar');
+    $slim->post('/cadastrarAluno','cadastrarAluno');
+    
+    $slim->run();
+    
+    // Funções    
+    require_once ('database/dao.class.php');
+    
+    function verificar() {
+	echo 'true';
+    }
+    
+    function cadastrarAluno() {
+        $request = \Slim\Slim::getInstance()->request();
+        $aluno = json_decode($request->getBody());
+        // Captura valores
+        $aluno->nome;
+        $aluno->media;
+        
+        // Revalida
+        // Enviar para o Banco de dados
+         $data_cadastro = array(
+            'nr_matricula' => $matricula,
+            'dt_nascimento' => $nascimento,
+            'cd_nivelescolar' => $nivel, 
+            /*'tp_sexo' => $sexo,*/
+            'nm_entrevistado' => $aluno
+        );
+        $dao = new dao_class();
+        $id = $dao->inserirEntrevistado($data_cadastro);
+        
+        echo json_encode($aluno);
+    }
+?>
