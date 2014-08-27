a3011b4965ec7b46a3ff6dbb63dfa56c4379b8a1
rhavy.maia@gmail.com <rhavy.maia@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2014-08-27 15:05:22 +0000

    WebService NutrIF

---
+++ trunk/NutrIF_service/database/connect.class.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once ('database/db.class.php');
+
+class connect_class {
+
+    var $db = null;
+
+    function connect_class() {
+
+        $this->db = new db_class;
+
+        if (!$this->db->connect('localhost', 'nutrif_user', 'nutr1f_us3r', 'novonutrif', false)) {
+                   echo "Não conectou no banco!";
+            $this->db->print_last_error(false);
+        }
+    }
+
+    public function database() {
+        if ($this->db != null) {
+            return $this->db;
+        }
+    }
+}
+?>
