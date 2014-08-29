5cdc3ad6c7ead00422c310f86d800789ba2daa2e
rhavy.maia <rhavy.maia@gmail.com>
2014-08-29 14:20:32 +0000

    Response via JSON

--- NutrIF_service/index.php
+++ NutrIF_service/index.php
@@ -1,6 +1,6 @@
 <?php
-
-    // Serviços
+    require_once './entidade/Server.class.php';
+    // Slim
     require '../Slim/Slim/Slim.php';
     \Slim\Slim::registerAutoloader();
 
@@ -9,35 +9,28 @@
     $slim->response()->header('Content-Type', 'application/json;charset=utf-8');
     
     $slim->get('/verificar', 'verificar');
-    $slim->post('/cadastrarAluno','cadastrarAluno');
+    $slim->post('/cadastrarAluno','cadastrarAluno');    
     
-    $slim->run();
-    
-    // Funções    
-    require_once ('database/dao.class.php');
-    
+    // Funções                   
     function verificar() {
-	echo 'true';
+        $server = new Server();
+        $server->online = true;
+        echoRespnse(201, $server);
+    }
+
+    function cadastrarAluno() {        
     }
     
-    function cadastrarAluno() {
-        $request = \Slim\Slim::getInstance()->request();
-        $aluno = json_decode($request->getBody());
-        // Captura valores
-        $aluno->nome;
-        $aluno->media;
-        
-        // Revalida
-        // Enviar para o Banco de dados
-         $data_cadastro = array(
-            'nr_matricula' => $matricula,
-            'dt_nascimento' => $nascimento,
-            'cd_nivelescolar' => $nivel,
-            'nm_entrevistado' => $aluno
-        );
-        $dao = new dao_class();
-        $id = $dao->inserirEntrevistado($data_cadastro);
-        
-        echo json_encode($aluno);
+    function echoRespnse($status_code, $response) {
+        $slim = \Slim\Slim::getInstance();
+        // Http response code
+        $slim->status($status_code);
+
+        // setting response content type to json
+        $slim->contentType('application/json');
+
+        echo json_encode($response);
     }
+
+    $slim->run();
 ?>
