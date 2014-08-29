bbdceddd0897ed606754948d886429c5aa839194
rhavy.maia <rhavy.maia@gmail.com>
2014-08-29 14:42:16 +0000

    Receber JSON e responder via JSON

--- NutrIF_service/index.php
+++ NutrIF_service/index.php
@@ -1,24 +1,33 @@
 <?php
-    require_once './entidade/Server.class.php';
+    // Entidades
+    require_once 'entidade/Server.class.php';
     // Slim
     require '../Slim/Slim/Slim.php';
     \Slim\Slim::registerAutoloader();
 
     $slim = new \Slim\Slim();
-    
-    $slim->response()->header('Content-Type', 'application/json;charset=utf-8');
-    
     $slim->get('/verificar', 'verificar');
     $slim->post('/cadastrarAluno','cadastrarAluno');    
     
+    function authenticate(\Slim\Route $route) {
+    }
+
     // Funções                   
     function verificar() {
         $server = new Server();
         $server->online = true;
+        
+        // Responder a requisição. Código HTTP (cabeçalho) e Entidade (Body - JSON).
         echoRespnse(201, $server);
     }
 
-    function cadastrarAluno() {        
+    function cadastrarAluno() {
+         $request = \Slim\Slim::getInstance()->request();
+         $body = $request->getBody();
+         $aluno = json_decode($body);
+         
+         // Persistir os dados no Banco.
+         echoRespnse(201, $aluno);
     }
     
     function echoRespnse($status_code, $response) {
