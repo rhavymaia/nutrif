3584bd4870c3b0b0896c890031169800846906a1
rhavy.maia <rhavy.maia@gmail.com>
2014-09-02 00:14:21 +0000

    Entidades: Anamnese.

---
+++ NutrIF_service/entidade/Anamnese.class.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Anamnese
+ *
+ * @author Rhavy
+ */
+class Anamnese {
+    
+    private $codigo;
+    
+    private $nutricionista;
+    
+    private $entrevistado;
+    
+    private $pesquisa;
+    
+    private $dtAnamnese;
+    
+    private $peso;
+    
+    private $altura;
+    
+    private $sexo;
+    
+    private $nivelEsporte;
+    
+    private $perfilAlimentar;
+    
+    public function __construct(){}
+    
+    public function getCodigo() {
+        return $this->codigo;
+    }
+
+    public function getNutricionista() {
+        return $this->nutricionista;
+    }
+
+    public function getEntrevistado() {
+        return $this->entrevistado;
+    }
+
+    public function getPesquisa() {
+        return $this->pesquisa;
+    }
+
+    public function getDtAnamnese() {
+        return $this->dtAnamnese;
+    }
+
+    public function getPeso() {
+        return $this->peso;
+    }
+
+    public function getAltura() {
+        return $this->altura;
+    }
+
+    public function getSexo() {
+        return $this->sexo;
+    }
+
+    public function getNivelEsporte() {
+        return $this->nivelEsporte;
+    }
+
+    public function getPerfilAlimentar() {
+        return $this->perfilAlimentar;
+    }
+
+    public function setCodigo($codigo) {
+        $this->codigo = $codigo;
+    }
+
+    public function setNutricionista($nutricionista) {
+        $this->nutricionista = $nutricionista;
+    }
+
+    public function setEntrevistado($entrevistado) {
+        $this->entrevistado = $entrevistado;
+    }
+
+    public function setPesquisa($pesquisa) {
+        $this->pesquisa = $pesquisa;
+    }
+
+    public function setDtAnamnese($dtAnamnese) {
+        $this->dtAnamnese = $dtAnamnese;
+    }
+
+    public function setPeso($peso) {
+        $this->peso = $peso;
+    }
+
+    public function setAltura($altura) {
+        $this->altura = $altura;
+    }
+
+    public function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+    public function setNivelEsporte($nivelEsporte) {
+        $this->nivelEsporte = $nivelEsporte;
+    }
+
+    public function setPerfilAlimentar($perfilAlimentar) {
+        $this->perfilAlimentar = $perfilAlimentar;
+    }
+}
+?>
