4bd0c1e95f6c1d9f6743e22d2a4c2bcbbdc6a06e
rhavy.maia@gmail.com <rhavy.maia@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2014-09-06 14:32:01 +0000

    - Implementação do serviço: cadastro do entrevistado; - Entidades.

---
+++ trunk/NutrIF_service/entidade/PerfilAlimentar.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Description of PerfilAlimentar
+ *
+ * @author Rhavy
+ */
+class PerfilAlimentar {
+    // Guia de bolso do Ministerio da saúde.
+    private $aluno;
+    
+    private $respostas;
+    
+    private $dataRegistro;
+}
+?>
