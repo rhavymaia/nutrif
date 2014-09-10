21425a74139ef3c5359dd17d00bbda1059229072
larissafelix.felix@gmail.com <larissafelix.felix@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2014-09-10 14:03:48 +0000

    Serviço de login

--- trunk/NutrIF_service/database/DbHandler.php
+++ trunk/NutrIF_service/database/DbHandler.php
@@ -1,5 +1,7 @@
 <?php
+
 require_once './util/constantes.php';
+
 /**
  * Class to handle all db operations
  * This class will have CRUD methods for database tables
@@ -13,70 +15,67 @@ class DbHandler {
 
     function __construct() {
         require_once dirname(__FILE__) . '/DbConnect.php';
-        
+
         // opening db connection
         $db = new DbConnect();
         $this->conn = $db->connect();
     }
-    
+
     /**
      * Inserir o usuário.
      * @param type $dados
      */
     public function inserirUsuario($aluno) {
-        
+
         //caso usuário não seja criado o valor 0 será atribuído
-         $cd_usuario = 0;
-         
+        $cd_usuario = 0;
+
         // insert query
         $stmt = $this->conn->prepare("INSERT INTO"
                 . " tb_usuario(nm_login, nm_senha, nm_usuario,"
                 . " dt_nascimento, cd_tipousuario, fl_ativo)"
-                . " values(?, ?, ?, ?, ".TP_ALUNO.", ".USUARIO_ATIVO.")");
-        
-        $nascimento = $data = implode("-",
-                array_reverse(explode("/",$aluno->nascimento)));
-        
+                . " values(?, ?, ?, ?, " . TP_ALUNO . ", " . USUARIO_ATIVO . ")");
+
+        $nascimento = $data = implode("-", array_reverse(explode("/", $aluno->nascimento)));
+
         // Parâmetros: tipos das entradas, entradas.
-        $stmt->bind_param("ssss", $aluno->login, $aluno->senha, $aluno->nome, 
-                $nascimento);
-        
-        $result = $stmt->execute();        
+        $stmt->bind_param("ssss", $aluno->login, $aluno->senha, $aluno->nome, $nascimento);
+
+        $result = $stmt->execute();
         if ($result) {
-            $cd_usuario = $stmt->insert_id; 
-        }        
+            $cd_usuario = $stmt->insert_id;
+        }
 
         $stmt->close();
-        
+
         return $cd_usuario;
     }
-    
+
     /**
      * Inserir entrevistado.
      * @param type $entrevistado
      * @return type
      */
     public function inserirEntrevistado($entrevistado) {
-        
+
         // insert query
         $stmt = $this->conn->prepare("INSERT INTO"
                 . " tb_entrevistado(cd_usuario, nr_matricula, cd_nivelescolar)"
                 . " values(?, ?, ?)");
-        
+
         // Parâmetros: tipos das entradas, entradas.
-        $stmt->bind_param("iii", $entrevistado->idUsuario, $entrevistado->matricula,
-                $entrevistado->nivel);
-        
-        $result = $stmt->execute();        
+        $stmt->bind_param("iii", $entrevistado->idUsuario, $entrevistado->matricula, $entrevistado->nivel);
+
+        $result = $stmt->execute();
         if ($result) {
-            $cd_entrevistado = $stmt->insert_id; 
-        }    
+            $cd_entrevistado = $stmt->insert_id;
+        }
 
         $stmt->close();
-        
+
         return $cd_entrevistado;
     }
-    
+
     /**
      * Descrição
      * @param type $login
@@ -84,20 +83,20 @@ class DbHandler {
      * @return type
      */
     function selectLogin($login, $senha) {
-        
+
         $sql = "SELECT usuario.nm_login, usuario.nm_usuario, cd_tipousuario, cd_usuario "
                 . " FROM tb_usuario AS usuario"
                 . " WHERE"
                 . " usuario.nm_login = ?"
                 . " AND usuario.nm_senha = ?";
 
-         $stmt = $this->conn->prepare($sql);
-         
+        $stmt = $this->conn->prepare($sql);
+
         // Parâmetros: tipos das entradas, entradas.
         $stmt->bind_param("ss", $login, $senha);
 
         $tupla = $stmt->fetchAll(PDO::FETCH_OBJ);
-        
+
         $usuario = null;
         if (isset($tupla)) {
             $usuario = new Usuario();
@@ -105,9 +104,11 @@ class DbHandler {
             $usuario->setCodigo($tupla['cd_usuario']);
             $usuario->setNome($tupla['nm_usuario']);
             $usuario->setTipoUsuario($tupla['cd_tipousuario']);
-        }                
-        
+        }
+
         return $usuario;
     }
+
 }
+
 ?>
