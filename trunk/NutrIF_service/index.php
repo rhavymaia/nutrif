e59dcc6f6b13d33bd270f00010845e7667dfd3d6
larissafelix.felix@gmail.com <larissafelix.felix@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2014-09-12 19:13:20 +0000

    Retirando duplicação da função cadastrarNutrionista.

--- trunk/NutrIF_service/index.php
+++ trunk/NutrIF_service/index.php
@@ -1,315 +1,265 @@
 <?php
-    // Entidades    
-    require_once 'database/DbHandler.php';
-    require_once 'util/constantes.php';
-    require_once 'entidade/Server.class.php';
-    require_once './entidade/Usuario.class.php';
-    require_once './entidade/Erro.php';
-    require_once './util/JsonUtil.php';
-    
-    // Slim
-    require '../Slim/Slim/Slim.php';
-    \Slim\Slim::registerAutoloader();
 
-    $slim = new \Slim\Slim();
-    $slim->get('/statusServer', 'statusServer');
-    $slim->post('/cadastrarAluno','cadastrarAluno');  
-    $slim->post('/cadastrarNutricionista','cadastrarNutricionista'); 
-    $slim->post('/analisarVCT','analisarVCT');
-    $slim->post('/calcularIMC','calcularIMC');    
-    $slim->post('/verificarLogin','verificarLogin');
-   
-    function authenticate(\Slim\Route $route) {
-    }
+// Entidades    
+require_once 'database/DbHandler.php';
+require_once 'util/constantes.php';
+require_once 'entidade/Server.class.php';
+require_once './entidade/Usuario.class.php';
+require_once './entidade/Erro.php';
+require_once './util/JsonUtil.php';
 
-    // Funções    
-    /**
-     * Verificar status do servidor.
-     */
-    function statusServer() {
-        $server = new Server();
-        $server->online = true;    
-        // Responder a requisição. Código HTTP (cabeçalho) e Entidade (Body - JSON).
-        echoRespnse(HTTP_CRIADO, $server);
-    }
+// Slim
+require '../Slim/Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
 
-    /**
-     * Cadastrar Aluno.
-     * @param $aluno
-     *  
-     *  {
-     *      nome: "valor",
-     *      login: "user@local.com",
-     *      senha: "valor",          
-     *      matricula: [1-9],
-     *      nascimento: "dd/mm/YYYY",
-     *      nivel: [1-3],
-     *      sexo: "M" | "F"         
-     *  }
-     *  
-     *  
-     * @return $aluno (http - 200)
-     *  {
-     *      idUsuario: [1-9],
-     *      idEntrevistado: [1-9]
-     *  }
-     * @return $erro (http - 400)
-     *  {
-     *      codigo: [1-9],
-     *      mensagem: "Erro"
-     * } 
-     * @author Rhavy Maia Guedes rhavy.maia@gmail.com
-     */
-    function cadastrarAluno() {
-        $request = \Slim\Slim::getInstance()->request();
-        $body = $request->getBody();        
-        $aluno = json_decode($body);
-        
-        // Persistir os dados no Banco.
-        $db = new DbHandler();
-        $cd_usuario = $db->inserirUsuario($aluno, TP_ALUNO);
-        
-        if($cd_usuario != 0){
-            $aluno->idUsuario = $cd_usuario;
-            $id_entrevistado = $db->inserirEntrevistado($aluno);
-            $aluno->idEntrevistado = $id_entrevistado;
-            // Resposta
-            echoRespnse(HTTP_CRIADO, $aluno);
-        }else{
-            $erro = new Erro();
-            $erro->codigo = 001;
-            $erro->mensagem = "Impossível criar usuário.";
-            echoRespnse(HTTP_ERRO_INTERNO, $erro);            
-        }        
-    }
-    
-    /**
-     * Cadastrar Nutricionista.
-     * @param $nutricionista
-     *  
-     *  {
-     *      nome: "valor",
-     *      login: "user@local.com",
-     *      senha: "valor",          
-     *      crn: [4-10],
-     *      siape: [8],
-     *      nascimento: "dd/mm/YYYY",
-     *      instituicao: [1-9]
-     *      sexo: "M" | "F"         
-     *  }
-     *  
-     *  
-     * @return $nutricionista (http - 200)
-     *  {
-     *      idUsuario: [1-9],
-     *      idNutricionista: [1-9]
-     *  }
-     * @return $erro (http - 400)
-     *  {
-     *      codigo: [1-9],
-     *      mensagem: "Erro"
-     * } 
-     * @author Larissa Félix larissafelix.felix@gmail.com
-     */
-    function cadastrarNutricionista() {
-        $request = \Slim\Slim::getInstance()->request();
-        $body = $request->getBody();        
-        $nutricionista = json_decode($body);
-        
-        // Persistir os dados no Banco.
-        $db = new DbHandler();
-        $cd_usuario = $db->inserirUsuario($nutricionista, TP_NUTRICIONISTA);
-        
-        if($cd_usuario != 0){
-            $nutricionista->idUsuario = $cd_usuario;
-            $id_nutricionista = $db->inserirNutricionista($nutricionista);
-            $nutricionista->idNutricionista = $id_nutricionista;
-            // Resposta
-            echoRespnse(HTTP_CRIADO, $nutricionista);
-        }else{
-            $erro = new Erro();
-            $erro->codigo = 001;
-            $erro->mensagem = "Impossivel criar usuario.";
-            echoRespnse(HTTP_ERRO_INTERNO, $erro);            
-        }        
-    }
-    
-    /**
-     * Cadastrar Nutricionista.
-     * @param $nutricionista
-     *  
-     *  {
-     *      nome: "valor",
-     *      login: "user@local.com",
-     *      senha: "valor",          
-     *      crn: [4-10],
-     *      siape: [8],
-     *      nascimento: "dd/mm/YYYY",
-     *      instituicao: [1-9]
-     *      sexo: "M" | "F"         
-     *  }
-     *  
-     *  
-     * @return $nutricionista (http - 200)
-     *  {
-     *      idUsuario: [1-9],
-     *      idNutricionista: [1-9]
-     *  }
-     * @return $erro (http - 400)
-     *  {
-     *      codigo: [1-9],
-     *      mensagem: "Erro"
-     * } 
-     * @author Larissa Félix larissafelix.felix@gmail.com
-     */
-    function cadastrarNutricionista() {
-        $request = \Slim\Slim::getInstance()->request();
-        $body = $request->getBody();        
-        $nutricionista = json_decode($body);
-        
-        // Persistir os dados no Banco.
-        $db = new DbHandler();
-        $cd_usuario = $db->inserirUsuario($nutricionista, TP_NUTRICIONISTA);
-        
-        if($cd_usuario != 0){
-            $nutricionista->idUsuario = $cd_usuario;
-            $id_nutricionista = $db->inserirNutricionista($nutricionista);
-            $nutricionista->idNutricionista = $id_nutricionista;
-            // Resposta
-            echoRespnse(HTTP_CRIADO, $nutricionista);
-        }else{
-            $erro = new Erro();
-            $erro->codigo = 001;
-            $erro->mensagem = "Impossivel criar usuario.";
-            echoRespnse(HTTP_ERRO_INTERNO, $erro);            
-        }        
-    }
-    
-    /**
-     * Analisar o Valor calórico total (VCT);
-     * 
-     * @param $aluno 
-     * 	{
-     *      'peso' : *[1-9].*[1-9],
-     *      'altura' : *[1-9].*[1-9],
-     *      'sexo' : 'M' | 'F',
-     *      'idade' : [1-9],
-     *      'nivelEsporte' : [1-4]
-     *  }
-     * 
-     * @return $vct 
-     * {
-     *      'vct' : *[1-9].*[1-9]
-     * }
-     */
-    function analisarVCT() {
-        $request = \Slim\Slim::getInstance()->request();
-        $body = $request->getBody();
-        $aluno = json_decode($body);
-        
-        $peso = $aluno->peso;
-        $alturaCm = ($aluno->altura * FATOR_CENTIMETRO);
-        $idade = $aluno->idade;
+$slim = new \Slim\Slim();
+$slim->get('/statusServer', 'statusServer');
+$slim->post('/cadastrarAluno', 'cadastrarAluno');
+$slim->post('/cadastrarNutricionista', 'cadastrarNutricionista');
+$slim->post('/analisarVCT', 'analisarVCT');
+$slim->post('/calcularIMC', 'calcularIMC');
+$slim->post('/verificarLogin', 'verificarLogin');
 
-        // Receber altura em metros e converter para centímetros.
-        if ($aluno->sexo == 'M') {
-            $tmb = 655 + (9.6 * $peso) + (1.8 * $alturaCm) 
-                    - (4.7 * $idade);            
-        } else {
-            $tmb = 655 + (14 * $peso) + (5 * $alturaCm) 
-                    - (6.7 * $idade);            
-        }
-        
-        // Construir o JSON de resposta.
-        $vct = array(
-                'vct' => ($tmb * $aluno->nivelEsporte)
-                );
-        
-        echoRespnse(HTTP_CRIADO, $vct);
+
+function authenticate(\Slim\Route $route) {
+    
+}
+
+// Funções    
+/**
+ * Verificar status do servidor.
+ */
+function statusServer() {
+    $server = new Server();
+    $server->online = true;
+    // Responder a requisição. Código HTTP (cabeçalho) e Entidade (Body - JSON).
+    echoRespnse(HTTP_CRIADO, $server);
+}
+
+/**
+ * Cadastrar Aluno.
+ * @param $aluno
+ *  
+ *  {
+ *      nome: "valor",
+ *      login: "user@local.com",
+ *      senha: "valor",          
+ *      matricula: [1-9],
+ *      nascimento: "dd/mm/YYYY",
+ *      nivel: [1-3],
+ *      sexo: "M" | "F"         
+ *  }
+ *  
+ *  
+ * @return $aluno (http - 200)
+ *  {
+ *      idUsuario: [1-9],
+ *      idEntrevistado: [1-9]
+ *  }
+ * @return $erro (http - 400)
+ *  {
+ *      codigo: [1-9],
+ *      mensagem: "Erro"
+ * } 
+ * @author Rhavy Maia Guedes rhavy.maia@gmail.com
+ */
+function cadastrarAluno() {
+    $request = \Slim\Slim::getInstance()->request();
+    $body = $request->getBody();
+    $aluno = json_decode($body);
+
+    // Persistir os dados no Banco.
+    $db = new DbHandler();
+    $cd_usuario = $db->inserirUsuario($aluno, TP_ALUNO);
+
+    if ($cd_usuario != 0) {
+        $aluno->idUsuario = $cd_usuario;
+        $id_entrevistado = $db->inserirEntrevistado($aluno);
+        $aluno->idEntrevistado = $id_entrevistado;
+        // Resposta
+        echoRespnse(HTTP_CRIADO, $aluno);
+    } else {
+        $erro = new Erro();
+        $erro->codigo = 001;
+        $erro->mensagem = "Impossível criar usuário.";
+        echoRespnse(HTTP_ERRO_INTERNO, $erro);
+    }
+}
+
+/**
+ * Cadastrar Nutricionista.
+ * @param $nutricionista
+ *  
+ *  {
+ *      nome: "valor",
+ *      login: "user@local.com",
+ *      senha: "valor",          
+ *      crn: [4-10],
+ *      siape: [8],
+ *      nascimento: "dd/mm/YYYY",
+ *      instituicao: [1-9]
+ *      sexo: "M" | "F"         
+ *  }
+ *  
+ *  
+ * @return $nutricionista (http - 200)
+ *  {
+ *      idUsuario: [1-9],
+ *      idNutricionista: [1-9]
+ *  }
+ * @return $erro (http - 400)
+ *  {
+ *      codigo: [1-9],
+ *      mensagem: "Erro"
+ * } 
+ * @author Larissa Félix larissafelix.felix@gmail.com
+ */
+function cadastrarNutricionista() {
+    $request = \Slim\Slim::getInstance()->request();
+    $body = $request->getBody();
+    $nutricionista = json_decode($body);
+
+    // Persistir os dados no Banco.
+    $db = new DbHandler();
+    $cd_usuario = $db->inserirUsuario($nutricionista, TP_NUTRICIONISTA);
+
+    if ($cd_usuario != 0) {
+        $nutricionista->idUsuario = $cd_usuario;
+        $id_nutricionista = $db->inserirNutricionista($nutricionista);
+        $nutricionista->idNutricionista = $id_nutricionista;
+        // Resposta
+        echoRespnse(HTTP_CRIADO, $nutricionista);
+    } else {
+        $erro = new Erro();
+        $erro->codigo = 001;
+        $erro->mensagem = "Impossivel criar usuario.";
+        echoRespnse(HTTP_ERRO_INTERNO, $erro);
+    }
+}
+
+/**
+ * Analisar o Valor calórico total (VCT);
+ * 
+ * @param $aluno 
+ * 	{
+ *      'peso' : *[1-9].*[1-9],
+ *      'altura' : *[1-9].*[1-9],
+ *      'sexo' : 'M' | 'F',
+ *      'idade' : [1-9],
+ *      'nivelEsporte' : [1-4]
+ *  }
+ * 
+ * @return $vct 
+ * {
+ *      'vct' : *[1-9].*[1-9]
+ * }
+ */
+function analisarVCT() {
+    $request = \Slim\Slim::getInstance()->request();
+    $body = $request->getBody();
+    $aluno = json_decode($body);
+
+    $peso = $aluno->peso;
+    $alturaCm = ($aluno->altura * FATOR_CENTIMETRO);
+    $idade = $aluno->idade;
+
+    // Receber altura em metros e converter para centímetros.
+    if ($aluno->sexo == 'M') {
+        $tmb = 655 + (9.6 * $peso) + (1.8 * $alturaCm) - (4.7 * $idade);
+    } else {
+        $tmb = 655 + (14 * $peso) + (5 * $alturaCm) - (6.7 * $idade);
     }
 
-    /**
-     * Descrição
-     * @param $imc
-     * {
-     *      'idUsuario' : [1-9],
-     *      'peso' : *[1-9].*[1-9],
-     *      'alturaCm' : *[1-9].*[1-9],
-     *      'idade': [1-9]
-     * }
-     */
-    function calcularIMC() {
-        $request = \Slim\Slim::getInstance()->request();
-        $body = $request->getBody();        
-        $imc = json_decode($body);
-        
-        $imc->idUsuario;
-        $imc->peso;
-        $imc->altura;
-        $imc->idade; 
-        
-        // Sugestão de OO.
-        $imcObjt = new Anamnese();
-        
-        // Implementar lógica do IMC.         
-    }
-    
-    /**
-     * Descrição
-     * @param $usuario
-     *  {
-     *      login:"valor",
-     *      senha:"valor"
-     *  }
-     * 
-     * @return $usuario HTTP-202
-     *  {
-     *      codigo: 21,
-     *      login: "user4@local.com",
-     *      nome:"João Silva",
-     *      tipoUsuario: "1",
-     *      ativo: TRUE | FALSE,
-     *  }
-     * @return $erro HTTP-400
-     */
-    function verificarLogin() {
-        $request = \Slim\Slim::getInstance()->request();
-        $body = $request->getBody();
-        $usuarioJson = json_decode($body);
+    // Construir o JSON de resposta.
+    $vct = array(
+        'vct' => ($tmb * $aluno->nivelEsporte)
+    );
 
-        $login = $usuarioJson->login;
-        $senha = $usuarioJson->senha;
+    echoRespnse(HTTP_CRIADO, $vct);
+}
 
-        $db = new DbHandler();
-        $usuario = $db->selectLogin($login, $senha);
-        
-        if (empty($usuario)) {            
-            $erro = new Erro();
-            $erro->codigo = 002;
-            $erro->mensagem = "Usuário não encontrado.";
-            
-            echoRespnse(HTTP_REQUISICAO_INVALIDA, $erro);
-        } else {
-            echoRespnse(HTTP_ACEITO, $usuario->toArray());
-        }
+/**
+ * Descrição
+ * @param $imc
+ * {
+ *      'idUsuario' : [1-9],
+ *      'peso' : *[1-9].*[1-9],
+ *      'alturaCm' : *[1-9].*[1-9],
+ *      'idade': [1-9]
+ * }
+ */
+function calcularIMC() {
+    $request = \Slim\Slim::getInstance()->request();
+    $body = $request->getBody();
+    $imc = json_decode($body);
+
+    $imc->idUsuario;
+    $imc->peso;
+    $imc->altura;
+    $imc->idade;
+
+    // Sugestão de OO.
+    $imcObjt = new Anamnese();
+
+    // Implementar lógica do IMC.         
+}
+
+/**
+ * Descrição
+ * @param $usuario
+ *  {
+ *      login:"valor",
+ *      senha:"valor"
+ *  }
+ * 
+ * @return $usuario HTTP-202
+ *  {
+ *      codigo: 21,
+ *      login: "user4@local.com",
+ *      nome:"João Silva",
+ *      tipoUsuario: "1",
+ *      ativo: TRUE | FALSE,
+ *  }
+ * @return $erro HTTP-400
+ */
+function verificarLogin() {
+    $request = \Slim\Slim::getInstance()->request();
+    $body = $request->getBody();
+    $usuarioJson = json_decode($body);
+
+    $login = $usuarioJson->login;
+    $senha = $usuarioJson->senha;
+
+    $db = new DbHandler();
+    $usuario = $db->selectLogin($login, $senha);
+
+    if (empty($usuario)) {
+        $erro = new Erro();
+        $erro->codigo = 002;
+        $erro->mensagem = "Usuário não encontrado.";
+
+        echoRespnse(HTTP_REQUISICAO_INVALIDA, $erro);
+    } else {
+        echoRespnse(HTTP_ACEITO, $usuario->toArray());
     }
-    
-      
-    function echoRespnse($status_code, $response) {
-        $slim = \Slim\Slim::getInstance();
-        // Http response code
-        $slim->response()->header('Content-Type', 
-                'application/json;charset=utf-8');
-        $slim->status($status_code);
-        // setting response content type to json
-        $slim->contentType('application/json');       
-        
-        // Chamada ao método estático para conversão de caracter UTF-8.
-        // Tranforma o array ou objeto em JSON.
-        echo json_encode(JsonUtil::utf8json($response));
-        
-        $slim->stop();
-    }
-    
+}
+
+
+
+function echoRespnse($status_code, $response) {
+    $slim = \Slim\Slim::getInstance();
+    // Http response code
+    $slim->response()->header('Content-Type', 'application/json;charset=utf-8');
+    $slim->status($status_code);
+    // setting response content type to json
+    $slim->contentType('application/json');
+
+    // Chamada ao método estático para conversão de caracter UTF-8.
+    // Tranforma o array ou objeto em JSON.
+    echo json_encode(JsonUtil::utf8json($response));
+
+    $slim->stop();
+}
+
 $slim->run();
 ?>
