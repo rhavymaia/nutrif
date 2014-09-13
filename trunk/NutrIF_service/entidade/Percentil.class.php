e8c58759bd33ed68616f651e940fd099b0510d48
rhavy.maia@gmail.com <rhavy.maia@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2014-09-13 16:42:45 +0000

    - Cálculo do Percentil.

--- trunk/NutrIF_service/entidade/Percentil.class.php
+++ trunk/NutrIF_service/entidade/Percentil.class.php
@@ -6,10 +6,57 @@
  * @author Rhavy
  */
 class Percentil {
+
+    private $vlPercentil;
+    private $cdPercentil;
+    private $sexo;
+    private $imc;
+    private $idadeMeses;
+   
+    public function getSexo() {
+        return $this->sexo;
+    }
+
+    public function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+    public function getVlPercentil() {
+        return $this->vlPercentil;
+    }
+
+    public function getCdPercentil() {
+        return $this->cdPercentil;
+    }
+
+    public function getImc() {
+        return $this->imc;
+    }
+
+    public function getIdadeMeses() {
+        return $this->idadeMeses;
+    }
+
+    public function setVlPercentil($vlPercentil) {
+        $this->vlPercentil = $vlPercentil;
+    }
+
+    public function setCdPercentil($cdPercentil) {
+        $this->cdPercentil = $cdPercentil;
+    }
+
+    public function setImc($imc) {
+        $this->imc = $imc;
+    }
+
+    public function setIdadeMeses($idadeMeses) {
+        $this->idadeMeses = $idadeMeses;
+    }
+
     
-    private $fator;
-    
-    private $imcPercentil;
-    
+    public function toArray() {
+        return get_object_vars($this);
+    }
 }
+
 ?>
