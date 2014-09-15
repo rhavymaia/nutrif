e7885c23c3e93bc391efef35fd8ed0864bf981e1
rhavy.maia <rhavy.maia@gmail.com>
2014-09-15 15:51:42 +0000

    - Alter table tb_usuario

--- NutriIF/configuration/novonutrif-altertable.sql
+++ NutriIF/configuration/novonutrif-altertable.sql
@@ -1,2 +1,9 @@
 -- 09/09/2014
 ALTER TABLE `tb_usuario` ADD `nm_sexo` VARCHAR(1) NOT NULL AFTER `dt_nascimento`;
+
+-- 13/09
+ALTER TABLE  `tb_nutricionista` ADD  `nm_siape` INT NOT NULL;
+ALTER TABLE  `tb_nutricionista` CHANGE  `nm_crn`  `nm_crn` INT NOT NULL;
+
+-- 14/09/2014
+ALTER TABLE `tb_usuario` ADD `vl_authkey` VARCHAR(32) NOT NULL AFTER `nm_senha`;
