c0d14cbc69bc41ef779bfb4c559dc44367e69177
rhavy.maia <rhavy.maia@gmail.com>
2014-11-27 14:03:51 +0000

    - Revisão do repositório.

--- nutrif_service/src/br/edu/ifpb/nutrif/entidade/Anamnese.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.nutrif.entidade;
-
-public class Anamnese {
-
-	public Anamnese() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- nutrif_service/src/br/edu/ifpb/nutrif/entidade/Nutricionista.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifpb.nutrif.entidade;
-
-public class Nutricionista {
-
-	public Nutricionista() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
