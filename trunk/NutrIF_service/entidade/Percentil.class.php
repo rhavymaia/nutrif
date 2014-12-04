97852dd919514dc365194c72c79d966309854ed6
rhavy.maia@gmail.com <rhavy.maia@gmail.com@891039b4-e853-02f8-3d8f-6ed31e14424c>
2014-12-04 23:04:30 +0000

    - Percentil com usuário.

--- trunk/NutrIF_service/entidade/Percentil.class.php
+++ trunk/NutrIF_service/entidade/Percentil.class.php
@@ -11,6 +11,8 @@ class Percentil {
     
     public $imc;
     
+    public $usuario;
+    
     public $vlPercentil;
     
     public $percentilMediano;
@@ -19,7 +21,62 @@ class Percentil {
     
     public $percentilSuperior;    
 
-    function __construct() {}   
-}
+    function __construct() {}
+    
+    function getCodigo() {
+        return $this->codigo;
+    }
 
+    function getImc() {
+        return $this->imc;
+    }
+
+    function getUsuario() {
+        return $this->usuario;
+    }
+
+    function getVlPercentil() {
+        return $this->vlPercentil;
+    }
+
+    function getPercentilMediano() {
+        return $this->percentilMediano;
+    }
+
+    function getPercentilInferior() {
+        return $this->percentilInferior;
+    }
+
+    function getPercentilSuperior() {
+        return $this->percentilSuperior;
+    }
+
+    function setCodigo($codigo) {
+        $this->codigo = $codigo;
+    }
+
+    function setImc($imc) {
+        $this->imc = $imc;
+    }
+
+    function setUsuario($usuario) {
+        $this->usuario = $usuario;
+    }
+
+    function setVlPercentil($vlPercentil) {
+        $this->vlPercentil = $vlPercentil;
+    }
+
+    function setPercentilMediano($percentilMediano) {
+        $this->percentilMediano = $percentilMediano;
+    }
+
+    function setPercentilInferior($percentilInferior) {
+        $this->percentilInferior = $percentilInferior;
+    }
+
+    function setPercentilSuperior($percentilSuperior) {
+        $this->percentilSuperior = $percentilSuperior;
+    }
+}
 ?>
