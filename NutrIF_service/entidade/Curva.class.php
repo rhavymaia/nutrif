c37285b03ad951fd5f6a7c2713ea2144644b3529
rhavy.maia <rhavy.maia@gmail.com>
2014-12-08 23:16:02 +0000

    - Perfil antropométrico.

---
+++ NutrIF_service/entidade/Curva.class.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Description of Curva
+ *
+ * @author Rhavy
+ */
+class Curva {
+    
+    public $entrevistado;
+    
+    public $imc;
+
+    public $percentilMediano;
+    
+    public $percentilInferior;
+    
+    public $percentilSuperior;
+
+    function __construct() {}
+
+    function getEntrevistado() {
+        return $this->entrevistado;
+    }
+
+    function getImc() {
+        return $this->imc;
+    }
+
+    function getPercentilMediano() {
+        return $this->percentilMediano;
+    }
+
+    function getPercentilInferior() {
+        return $this->percentilInferior;
+    }
+
+    function getPercentilSuperior() {
+        return $this->percentilSuperior;
+    }
+
+    function setEntrevistado($entrevistado) {
+        $this->entrevistado = $entrevistado;
+    }
+
+    function setImc($imc) {
+        $this->imc = $imc;
+    }
+
+    function setPercentilMediano($percentilMediano) {
+        $this->percentilMediano = $percentilMediano;
+    }
+
+    function setPercentilInferior($percentilInferior) {
+        $this->percentilInferior = $percentilInferior;
+    }
+
+    function setPercentilSuperior($percentilSuperior) {
+        $this->percentilSuperior = $percentilSuperior;
+    }    
+}
